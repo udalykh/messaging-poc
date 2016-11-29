6f59e03b88f9d5f7447104f90a87795a89ca79e7
Alexey Udalykh <udalykh@newstandards-us.com>
2016-11-29 15:41:50 +0800

    added  logging

--- colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
+++ colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
@@ -1,6 +1,8 @@
 package com.newstandards.tsesna.bi;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,11 +20,14 @@ import javax.jms.ConnectionFactory;
 @Configuration
 public class AppConfig {
 
+    private static Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
     /**
      * JMS connection factory based on internal Apache Active MQ message broker
      */
     @Bean
     public ConnectionFactory connectionFactory(@Value("${brokerURL}") String brokerURL) {
+        logger.info("Creating connection factory with broker URL: {}", brokerURL);
         return new CachingConnectionFactory(new ActiveMQConnectionFactory(brokerURL));
     }
 }
