11c22ba139afec4d9ec93fea7e71347b7cdb4a8c
Nikolay Kopylov <kopylov@newstandards-us.com>
2016-11-30 16:32:58 +0800

    IBM Mq support

--- colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
+++ colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
@@ -36,7 +36,7 @@ public class AppConfig {
 	    cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, "QM1");
 	    cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
 	    cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, "localhost");
-	    cf.setStringProperty(WMQConstants.WMQ_CHANNEL, "SYSTEM.DEF.SVRCONN");
+	    cf.setStringProperty(WMQConstants.WMQ_CHANNEL, "SYSTEM.ADMIN.SVRCONN");
 	    cf.setIntProperty(WMQConstants.WMQ_PORT, 2001);
 
         return cf;
