9edadb9265d3e9a27d7eba801bbf22f64a6e3925
Nikolay Kopylov <kopylov@newstandards-us.com>
2016-11-30 14:53:05 +0800

    IBM Mq support

--- colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
+++ colvir-adapter-app/src/main/java/com/newstandards/tsesna/bi/AppConfig.java
@@ -1,6 +1,8 @@
 package com.newstandards.tsesna.bi;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import com.ibm.msg.client.jms.JmsConnectionFactory;
+import com.ibm.msg.client.jms.JmsFactoryFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.jms.connection.CachingConnectionFactory;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 
 /**
  * Simplistic app configuration creating an embedded JMS connection factory.
@@ -26,8 +28,16 @@ public class AppConfig {
      * JMS connection factory based on internal Apache Active MQ message broker
      */
     @Bean
-    public ConnectionFactory connectionFactory(@Value("${brokerURL}") String brokerURL) {
+    public ConnectionFactory connectionFactory(@Value("${brokerURL}") String brokerURL) throws JMSException {
         logger.info("Creating connection factory with broker URL: {}", brokerURL);
-        return new CachingConnectionFactory(new ActiveMQConnectionFactory(brokerURL));
+
+        JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
+	    JmsConnectionFactory cf = ff.createConnectionFactory();
+	    cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, "QM1");
+	    cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+	    cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, "localhost");
+	    cf.setIntProperty(WMQConstants.WMQ_PORT, 1414);
+
+        return cf;
     }
 }
